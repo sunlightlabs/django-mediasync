c91ee2772322900857681e6878ac65975155fa3d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-10-06 17:25:23 -0400

    move s3 to clients directory

--- mediasync/__init__.py
+++ mediasync/__init__.py
@@ -35,7 +35,7 @@ def sync(bucket=None, prefix=''):
     """
     
     from django.conf import settings
-    from mediasync import s3
+    from mediasync.clients import s3
     import cStringIO
     
     assert hasattr(settings, "MEDIA_ROOT")
