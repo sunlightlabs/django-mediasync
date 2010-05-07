dd5e795fb3f8a3d7d4c298294f16ff12a530fd6e
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-05-07 16:30:46 -0400

    when DEBUG = False, redirect local media URLs to remote

--- mediasync/urls.py
+++ mediasync/urls.py
@@ -1,9 +1,16 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 
-if (settings.DEBUG):
-    from mediasync import backends
-    client = backends.client()
-    urlpatterns = patterns('',  
-        url(r'^%s/(?P<path>.*)$' % client.local_media_url.strip('/'), 'django.views.static.serve', {'document_root': client.media_root}),
-    )
+from mediasync import backends
+client = backends.client()
+
+if settings.DEBUG:
+    view = 'django.views.static.serve'
+    params = {'document_root': client.media_root}
+else:
+    view = 'django.views.generic.simple.redirect_to'
+    params = {'url': client.remote_media_url().strip('/') + '/%(path)s'}
+
+urlpatterns = patterns('',  
+    url(r'^%s/(?P<path>.*)$' % client.local_media_url.strip('/'), view, params),
+)
