b1ddd93b9ee3a21a9093e8cb8a64e8f690439820
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-10-25 17:01:32 -0400

    add force option support back into s3 sync

--- mediasync/backends/s3.py
+++ mediasync/backends/s3.py
@@ -94,7 +94,7 @@ class Client(BaseClient):
             key = Key(self._bucket)
             key.key = remote_path
         
-        if key.get_metadata('mediasync-checksum') != raw_b64digest:
+        if force or key.get_metadata('mediasync-checksum') != raw_b64digest:
             
             key.set_metadata('mediasync-checksum', raw_b64digest)
             key.set_contents_from_string(filedata, headers=headers, md5=(hexdigest, b64digest))
