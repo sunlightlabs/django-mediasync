3aba1006b77ae11c2a5123847aff8e4aa6046509
Jonathan Drosdeck <Jon.Drosdeck@2sprout.com>
2010-11-23 11:00:38 -0500

    Modified setup.py to properly install all tests (css, js) on OS X

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=find_packages(),
     package_data = {
         'mediasync': [
-            'tests/media/*',
+            'tests/media/*/*',
         ]
     },
     license='BSD License',
